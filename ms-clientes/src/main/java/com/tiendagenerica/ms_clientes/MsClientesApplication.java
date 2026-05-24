03a44596733c33536f2b92c8a9a323d7fab8db93
ryunITShere <martinezsuarezjuandiego6@gmail.com>
2026-05-23 22:24:06 -0500

    correcciones railway1

--- ms-clientes/src/main/java/com/tiendagenerica/ms_clientes/MsClientesApplication.java
+++ ms-clientes/src/main/java/com/tiendagenerica/ms_clientes/MsClientesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tiendagenerica")
 public class MsClientesApplication {
 
 	public static void main(String[] args) {
